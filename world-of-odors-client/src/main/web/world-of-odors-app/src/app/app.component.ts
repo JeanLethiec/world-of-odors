3cfdac343418aa933b3105cb7362ad67172e9256
Jean <jean.lethiec@gmail.com>
2020-05-27 16:44:43 +0200

    First render of a world map

--- world-of-odors-client/src/main/web/world-of-odors-app/src/app/app.component.ts
+++ world-of-odors-client/src/main/web/world-of-odors-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone } from "@angular/core";
 import * as am4core from "@amcharts/amcharts4/core";
-import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 am4core.useTheme(am4themes_animated);
@@ -11,51 +12,28 @@ am4core.useTheme(am4themes_animated);
   styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  private chart: am4charts.XYChart;
+  private map: am4maps.MapChart;
 
   constructor(private zone: NgZone) {}
 
   ngAfterViewInit() {
     this.zone.runOutsideAngular(() => {
-      let chart = am4core.create("chartdiv", am4charts.XYChart);
+      let map = am4core.create("chartdiv", am4maps.MapChart);
+      map.geodata = am4geodata_worldLow;
+      map.projection = new am4maps.projections.Miller();
 
-      chart.paddingRight = 20;
+      let worldSeries = map.series.push(new am4maps.MapPolygonSeries());
+      worldSeries.exclude = ["AQ"];
+      worldSeries.useGeodata = true;
 
-      let data = [];
-      let visits = 10;
-      for (let i = 1; i < 366; i++) {
-        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-        data.push({ date: new Date(2018, 0, i), name: "name" + i, value: visits });
-      }
-
-      chart.data = data;
-
-      let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-      dateAxis.renderer.grid.template.location = 0;
-
-      let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      valueAxis.tooltip.disabled = true;
-      valueAxis.renderer.minWidth = 35;
-
-      let series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.dateX = "date";
-      series.dataFields.valueY = "value";
-
-      series.tooltipText = "{valueY.value}";
-      chart.cursor = new am4charts.XYCursor();
-
-      let scrollbarX = new am4charts.XYChartScrollbar();
-      scrollbarX.series.push(series);
-      chart.scrollbarX = scrollbarX;
-
-      this.chart = chart;
+      this.map = map;
     });
   }
 
   ngOnDestroy() {
     this.zone.runOutsideAngular(() => {
-      if (this.chart) {
-        this.chart.dispose();
+      if (this.map) {
+        this.map.dispose();
       }
     });
   }
